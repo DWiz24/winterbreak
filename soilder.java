97d53bf678a573ff4d09b47e55ca9918801ccab1
DWiz24 <bobthequark@gmail.com>
2016-12-19 12:15:52 -0500

    Added pseudocode for my soldiers, will implement later today

--- soilder.java
+++ soilder.java
@@ -3,6 +3,24 @@ package winterbreak;
 import battlecode.common.*;
 
 public class soilder {
+	/*Soldier priorities
+	 * if canAttack:
+	 *    Shoot the closest thing that can attack
+	 * if enemies and zombies: 
+	 *    stay out of range of zombies
+	 * if zombies:
+	 *    move out of range of zombies
+	 * if enemies:
+	 *    move in as long as they can't shoot you
+	 *    if 3x move visible friends than enemies blip to rush
+	 *    if you recieve blip rush
+	 * if nothing:
+	 *    id%2==0: 
+	 *       nav to scout location
+	 *    id%2==1:
+	 *       stay near archons
+	 *    
+	 */
 	static void run(RobotController rc)throws Exception {
 		
 		
