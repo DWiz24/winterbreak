bf728c25338d221afc6cc58fa786c9f8e8621a4f
etherlyt <hcx2001@gmail.com>
2016-12-29 00:14:16 -0500

    methods shared by 2 or more robots, strategies?

---
+++ CommonFunc.java
@@ -0,0 +1,23 @@
+package winterbreak;
+
+import battlecode.common.*;
+
+public class CommonFunc {
+	//Functions for all robot types
+	
+	/*
+	 * Only called if infected:
+	 * 	If health is high or enemies not around [survival chance high]:
+	 * 		end; [continue on - no suicide]
+	 * 	Use distances from enemies & compare to turns till infection end
+	 *  Move toward enemies
+	 *  If by multiple infected:
+	 *  	based on till infection approach closest enemy
+	 *  	If by enemy archon or base:
+	 *  		ignore closest robots 
+	 */
+	public static void InfectSuicide( RobotController rc ) {
+		
+		
+	}
+}
